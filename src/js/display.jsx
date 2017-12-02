40da1bc087feea74f5ab5bf5ae6111a21a36abba
Martin Lärka <martin.larka@gmail.com>
2017-12-02 22:34:13 +0100

    Labels on line chart

--- src/js/display.jsx
+++ src/js/display.jsx
@@ -122,7 +122,7 @@ class DisplayBudget extends React.Component{
 						<LineChart width={730} height={250} data={this.mapLineData(result)}
 							margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
 							<CartesianGrid strokeDasharray="3 3" />
-							<XAxis dataKey="name" />
+							<XAxis dataKey="date" />
 							<YAxis />
 							<Tooltip />
 							<Legend />
